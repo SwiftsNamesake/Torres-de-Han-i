bfed655026af6992211ddb6fecb88c27ac29623c
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-11-04 19:08:10 +0100

    1) Added joke (very important update) 2) Added dot grid enhancements (DGEs)

--- recreation.hs
+++ recreation.hs
@@ -50,11 +50,11 @@ cutAt x list = let (a, b) = break (== x) list in (a, tail b)
 -- TODO | Extract 'split' routine (groupBy (/= ';'))
 -- TODO | Error handling
 -- TODO | Extract pure code (string parsing)
+-- TODO | let there = be light in heaven
 loadMap :: String -> IO Map
 loadMap fn = do
 	file <- readFile fn
 	let tokens = map (filter (/= ';')) . groupBy (\ a b -> b /= ';') . filter (not . isSpace) $ file
-	print tokens
 	let points = map (\ str -> let (x, y) = cutAt ',' str in (read x, -read y)) $ tokens
 	return points
 
@@ -64,15 +64,19 @@ loadMap fn = do
 -------------------------------------------------------------------------
 main :: IO ()
 main = do
-	markers <- loadMap $ ["sweden.txt", "generated.txt", "norway.txt"] !! 2
-	print markers
+	markers <- loadMap $ ["sweden.txt", "generated.txt", "norway.txt", "suomi.txt"] !! 3
 	animate window white $ render markers
 	where
 		window 				= [ InWindow "Cartographer" size (25, 25), FullScreen (450, 450)] !! 0
-		render markers dt 	= pictures $ [dotGrid 20 20 32]  --[outline markers, silhouette markers, dots markers 0 0, dots markers dx 0]
+		--render markers dt 	= pictures $ [dotGrid 20 20 $ 8+16*(abs . sin $ 1/8*dt)]  --[outline markers, silhouette markers, dots markers 0 0, dots markers dx 0]
+		render markers dt 	= pictures $ [spiral, dotGrid 20 20 $ (8+) . (16*) . abs . sin $ 1/8*dt]  --[outline markers, silhouette markers, dots markers 0 0, dots markers dx 0]
 		outline markers 	= scale 0.5 0.5 . color black $ lineLoop markers
 		silhouette markers	= scale 0.5 0.5 . color (makeColor 0.35 0.45 0.82 0.7) . translate dx 0 $ polygon markers
 		dots markers dx dy	= scale 0.5 0.5 . pictures $ map (\(x,y) -> color (makeColor (sin x) (cos y) 0.86 1.0) . translate (dx+x) (dy+y) $ circleSolid 5) markers
 		dx 					= 300
 		size 				= (700, 700)
-		dotGrid rws cls pad = pictures [ color (makeColor (sin (r*pad)) (cos (c*pad)) 0.86 1.0) . translate (r*pad) (c*pad) $ circleSolid 5 | r <- [1..rws], c <- [1..cls] ]
+		grid f rws cls		= [f r c | r <- [1..rws], c <- [1..cls] ] -- TODO | Zero-index (?)
+		dotGrid rws cls pad = pictures $ grid (\ r c -> color (makeColor (sin (r*pad*1/8)) (cos (c*pad*1/8)) 0.86 1.0)
+																. translate (c*pad) (r*pad) $ circleSolid (5 + 3 * (sin $ c*pad))) rws cls
+		spiral = pictures [rotate (n*5) . translate (n*2) 0 $ circleSolid 5 | n <- [1..320]]
+		 --spiral, complex numbers, fibonacci
